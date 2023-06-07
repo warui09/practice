1bcc57bf2f12612947d428c4cffb1802b61315e9
Kevin Warui <waruimacharia@gmail.com>
2023-06-07 14:38:57 +0300

    Add main.h

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <unistd.h>
+
+#endif /* MAIN_H */
